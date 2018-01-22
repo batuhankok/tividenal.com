09735d13ea18c70727f03db93496775df8adeba0
Batuhan Kök <batuhankok@gmail.com>
2018-01-22 18:40:47 +0300

    Add tags input and autocomplete(!)

--- tividenal.js
+++ tividenal.js
@@ -1,9 +1,22 @@
 
-var app = angular.module('Tividenal', ['angularUtils.directives.dirPagination']);
+var app = angular.module('Tividenal', ['angularUtils.directives.dirPagination','ngTagsInput']);
 
 function ItemsController($scope, $http) {
   
   $scope.items = null;
+
+  $scope.tags = [
+    'ürün'
+  ];
+
+  var url = "tags.json";
+  $http.get(url).success(function (response) {
+      $scope.tagsJSON = response;
+  });
+
+  $scope.loadTags = function(query) {
+    return $scope.tagsJSON;
+  };
 
   var url = "items.json";
   $http.get(url).success(function (response) {
@@ -15,11 +28,9 @@ function ItemsController($scope, $http) {
 
   $scope.items = null;
 
-  console.log($("#q").val());
-
   $scope.tag = function(message) {
     if ($scope.tags) {
-      return $scope.tags.replace(/\s*,\s*/g, ',').split(',').every(function(tag) {
+      return $scope.tags.every(function(tag) {
         return message.tags.some(function(objTag){
           return objTag.indexOf(tag) !== -1;
         });
@@ -28,10 +39,10 @@ function ItemsController($scope, $http) {
       return true;
     }
   };
-
+  
 }
 
-function PaginationController($scope) { }
+function PaginationController($scope) {};
 
 app.controller('ItemsController', ItemsController);
 app.controller('PaginationController', PaginationController);
